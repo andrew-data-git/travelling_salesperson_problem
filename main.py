7130bac078be1967a9bf4825463f595e21b9e362
andrew-data-git <119108096+andrew-data-git@users.noreply.github.com>
2023-09-23 17:12:14 +0100

    Add files via upload
    Removed scratch file
    Added cost to title
    Improved comments and code format

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import utils
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    points = utils.points_generator(200, 0, 5000) # defaults are num_points = 10, min = 0, max = 100
+    points = utils.points_generator(200, 0, 5000)  # defaults are num_points = 10, min = 0, max = 100
     travel = tsp.TravellingSalesperson(points, verbose=False)
     travel.run_tsp("greedy")
